0ee626b10a72fc4cde0a8aabfe4d7b294fd50d22
vortexory <robothook001231@gmail.com>
2025-04-26 20:44:41 -0400

    fix

--- server/index.ts
+++ server/index.ts
@@ -61,7 +61,7 @@ app.use((req, res, next) => {
   // It is the only port that is not firewalled.
   const port = 5000;
   server.listen({
-    port,
+    port, 
     host: "https://cleveraisaas.vercel.app/"
   }, () => {
     log(`serving on port ${port}`);
